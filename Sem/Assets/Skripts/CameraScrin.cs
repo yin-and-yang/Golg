edd2715f323cd2d9ecd57b322a9f0795cf7883de
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-02-04 18:11:48 +0200

    roteihen

---
+++ Sem/Assets/Skripts/CameraScrin.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScrin : MonoBehaviour {
+
+    public GameObject pl1;
+    public GameObject mainCam;
+    public List<GameObject>Controls;
+    Camera myCam;
+    public bool isFerst = true;
+    public bool isMin = false;
+
+    private float X;
+    private float Y;
+    private float Z;
+
+
+ 
+    
+
+
+    float start;
+    public float distants;
+    public float Withs;
+    public float He;
+    public virtual void FollowMod()
+    {
+        isFerst = !isFerst;
+    }
+    public virtual void IsMaxMod()
+    {
+        isMin = !isMin;
+
+        if(isMin)
+        {
+
+            myCam.orthographicSize = distants;
+            foreach (GameObject e in Controls)
+                e.active = false;
+        }
+        else
+        {
+            myCam.orthographicSize = start;          
+            foreach (GameObject e in Controls)
+                e.active = true;
+
+        }
+    }
+
+    private void Start()
+    {
+        
+        
+      //  Controls1.Add(temp)
+
+
+        X = transform.position.x;
+        Y = transform.position.y;
+        Z = transform.position.z;
+
+        myCam =GetComponent<Camera>();
+        start = myCam.orthographicSize;
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+    }
+    // Update is called once per frame
+    
+    void Update () {
+
+
+       
+
+                transform.position = new Vector3(X + pl1.transform.position.x + He, Y + pl1.transform.position.y + Withs, Z + pl1.transform.position.z);
+       
+
+    }
+}
---
+++ Sem/Assets/Skripts/TDA_Camera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDA_Camera : MonoBehaviour {
+    public GameObject Player;
+    public float offsetX = 0;
+    public float offsetZ = -5;
+    public float PlayerVelocity = 5;
+    private float movementX;
+    private float movementZ;
+
+
+    public float RoffsetX = 0;
+    public float RoffsetZ = -5;
+    private float RmovementX;
+    private float RmovementZ;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        movementX = ((Player.transform.position.x + offsetX - this.transform.position.x));
+        movementZ = ((Player.transform.position.z + offsetZ - this.transform.position.z));
+
+        RmovementX = ((Player.transform.rotation.x + RoffsetX - this.transform.rotation.x));
+        RmovementZ = ((Player.transform.rotation.z + RoffsetZ - this.transform.rotation.z));
+
+        this.transform.position += new Vector3((movementX * PlayerVelocity * Time.deltaTime), 0, (movementZ * PlayerVelocity * Time.deltaTime));
+        //this.transform.rotation = new Quaternion(RmovementX,0, RmovementZ,0);
+
+    }
+}
